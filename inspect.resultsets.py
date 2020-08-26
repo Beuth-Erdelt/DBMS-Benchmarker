72f06f7d8ec4f8101e57d4ad1ea7fd81e5892753
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-26 13:43:42 +0200

    Legal notice in source code

--- inspect.resultsets.py
+++ inspect.resultsets.py
@@ -1,10 +1,19 @@
 """
-:Date: 2020-03-18
-:Version: 0.1
-:Authors: Patrick Erdelt
+    Small demo how to inspect result sets of the Python Package DBMS Benchmarker
+    Copyright (C) 2020  Patrick Erdelt
 
-Small demo for inspection of finished benchmarks.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from dbmsbenchmarker import *
 import pandas as pd
