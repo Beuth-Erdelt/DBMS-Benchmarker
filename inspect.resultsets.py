472b5e966032bc0d2518b19e8c45b2f8b741d8d4
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-28 12:48:52 +0200

    Tool: Inspect difference in result sets improved a bit

--- inspect.resultsets.py
+++ inspect.resultsets.py
@@ -1,5 +1,7 @@
 """
-    Small demo how to inspect result sets of the Python Package DBMS Benchmarker
+    Small demo how to inspect result sets of the Python Package DBMS Benchmarker.
+    This prints the first difference in result sets for each query in the latest benchmark in the current folder.
+    TODO: Improve usability and flexibility
     Copyright (C) 2020  Patrick Erdelt
 
     This program is free software: you can redistribute it and/or modify
@@ -46,26 +48,39 @@ evaluate.load_experiment(code)
 from operator import itemgetter
 
 list_connections = evaluate.get_experiment_list_connections()
-numQuery=1
-numRun=0
+list_queries = evaluate.get_experiment_list_queries()
+#list_queries = [1]
 
-df = evaluate.get_datastorage_df(numQuery, numRun)
-data = df.values
-df = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df.columns)
-
-df2={}
-list_warnings = evaluate.get_warning(numQuery)
-print(list_warnings)
-for c in list_connections:
-    if c in list_warnings and len(list_warnings[c])>0:
-         df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
-         data = df_tmp.values
-         df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df_tmp.columns)
-
-
-for c,d in df2.items():
-    print(c)
-    inspector.getDifference12(d,df)
-    inspector.getDifference12(df,d)
-
+for numQuery in list_queries:
+    list_warnings = evaluate.get_warning(numQuery)
+    #print("Q"+str(numQuery))
+    #print(list_warnings)
+    numRun=0
+    df = evaluate.get_datastorage_df(numQuery, numRun)
+    data = df.values
+    if len(data) > 0 and sum([len(v) for k,v in list_warnings.items()]) > 0:
+        print("===Q{}===".format(numQuery))
+        print(list_warnings)
+        df = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df.columns)
+        for c in list_connections:
+            if c in list_warnings and len(list_warnings[c])>0:
+                s = evaluate.benchmarks.protocol['query'][str(numQuery)]['dataStorage']
+                r = evaluate.benchmarks.protocol['query'][str(numQuery)]['resultSets']
+                for numRun, result in enumerate(s):
+                    df2={}
+                    data_stored = s[numRun]
+                    print("numRun: "+str(numRun))
+                    #print(data)
+                    for c, result_diff in r.items():
+                        if len(result_diff) > 0 and r[c][numRun] != data_stored:
+                             df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
+                             data = df_tmp.values
+                             df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df_tmp.columns)
+                    for c,d in df2.items():
+                        print("Storage has more than {}:".format(c))
+                        print(inspector.getDifference12(d,df))
+                        print(c+" has more than storage")
+                        print(inspector.getDifference12(df,d))
+                    if len(df2) > 0:
+                        break
 
