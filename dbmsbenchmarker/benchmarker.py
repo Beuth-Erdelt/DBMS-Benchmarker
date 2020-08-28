e205c2e973804be609f8f20e01a3f272af5218c4
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-28 08:49:54 +0200

    Evaluation: Catch missing result sets

--- dbmsbenchmarker/benchmarker.py
+++ dbmsbenchmarker/benchmarker.py
@@ -1437,10 +1437,11 @@ class benchmarker():
 			print(str(sys.getsizeof(self.protocol['query'][str(numQuery)]['dataStorage']))+" bytes")
 	def readDataStorage(self, query, numRun=0):
 		df = pd.DataFrame(self.protocol['query'][str(query)]['dataStorage'][numRun])
-		# set column names
-		df.columns = df.iloc[0]
-		# remove first row
-		df = df[1:]
+		if not df.empty:
+			# set column names
+			df.columns = df.iloc[0]
+			# remove first row
+			df = df[1:]
 		return df
 	def readResultSet(self, query, connection, numRun=0):
 		df = pd.DataFrame(self.protocol['query'][str(query)]['resultSets'][connection][numRun])
