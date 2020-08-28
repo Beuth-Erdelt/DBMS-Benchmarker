539c975f31f716fbfb5c259b1eb8b959047dfda8
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-08-28 14:27:47 +0200

    Legal notice in source code

--- demo-interactive-inspection-legacy.py
+++ demo-interactive-inspection-legacy.py
@@ -1,10 +1,20 @@
 """
-:Date: 2018-02-03
-:Version: 0.1
-:Authors: Patrick Erdelt
+    Small demo for inspection of finished benchmarks and manual sending of queries in an interactive python shell.
+    This uses the old interface.
+    Copyright (C) 2020  Patrick Erdelt
 
-Small demo for inspection of finished benchmarks and manual sending of queries in an interactive python shell.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from dbmsbenchmarker import *
 
