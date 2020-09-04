7770b9bc0b5d9f70c3d1267a5a9befa2c026a37b
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2020-09-04 09:03:34 +0200

    Tool: Inspect difference in result sets args query and number of run

--- inspect.resultsets.py
+++ inspect.resultsets.py
@@ -30,6 +30,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='A benchmark tool for RDBMS. It connects to a given list of RDBMS via JDBC and runs a given list benchmark queries. Optionally some reports are generated.')
 parser.add_argument('-r', '--result-folder', help='folder for storing benchmark result files, default is given by timestamp', default="./")
+parser.add_argument('-q', '--query', help='number of query to inspect', default=None)
+parser.add_argument('-n', '--num-run', help='number of run to inspect', default=None)
 
 args = parser.parse_args()
 
@@ -58,12 +60,20 @@ list_queries = evaluate.get_experiment_list_queries()
 #list_queries = [1]
 
 for numQuery in list_queries:
+    if args.query is not None and int(args.query) != numQuery:
+        continue
+    query = tools.query(evaluate.benchmarks.queries[numQuery-1])
+    #print("PRECISION:")
+    #print(query.restrict_precision)
     list_warnings = evaluate.get_warning(numQuery)
     #print("Q"+str(numQuery))
     #print(list_warnings)
     numRun=0
     df = evaluate.get_datastorage_df(numQuery, numRun)
     data = df.values
+    #print(data)
+    #data = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in data]
+    #print(data)
     if len(data) > 0 and sum([len(v) for k,v in list_warnings.items()]) > 0:
         print("===Q{}===".format(numQuery))
         print(list_warnings)
@@ -73,15 +83,30 @@ for numQuery in list_queries:
                 s = evaluate.benchmarks.protocol['query'][str(numQuery)]['dataStorage']
                 r = evaluate.benchmarks.protocol['query'][str(numQuery)]['resultSets']
                 for numRun, result in enumerate(s):
+                    if args.num_run is not None and int(args.num_run) != numRun:
+                        continue
                     df2={}
                     data_stored = s[numRun]
                     print("numRun: "+str(numRun))
                     #print(data)
-                    for c, result_diff in r.items():
-                        if len(result_diff) > 0 and r[c][numRun] != data_stored:
-                             df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
-                             data = df_tmp.values
-                             df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df_tmp.columns)
+                    s2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in data_stored]
+                    #print(s2)
+                    r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in r[c][numRun]]
+                    #print(r2)
+                    #for c, result_diff in r.items():
+                    if len(r2) > 0 and r2 != s2:# r[c][numRun] != data_stored:
+                        #print(c)
+                        #s2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in data_stored]
+                        #print(s2)
+                        #r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in r[c][numRun]]
+                        #print(r2)
+                        #exit()
+                        df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
+                        data = df_tmp.values
+                        r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else item for item in sublist] for sublist in data]
+                        #print(r2)
+                        data = r2
+                        df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df_tmp.columns)
                     for c,d in df2.items():
                         print("Storage has more than {}:".format(c))
                         print(inspector.getDifference12(d,df))
