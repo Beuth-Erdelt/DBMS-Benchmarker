b600cf84ab2062827a6a9bd0375447ba39f2901b
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2021-01-22 16:32:00 +0100

    Tool: Inspect resultsets - optionally remove titles of result sets when comparing, optionally show diff

--- evaluate.py
+++ evaluate.py
@@ -46,6 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--code', help='code of experiment', default="")
     parser.add_argument('-q', '--query', help='number of query to inspect', default=None)
     parser.add_argument('-n', '--num-run', help='number of run to inspect', default=None)
+    parser.add_argument('-d', '--diff', help='show differences in result sets', action='store_true', default=False)
+    parser.add_argument('-rt', '--remove-titles', help='remove titles when comparing result sets', action='store_true', default=False)
     parser.add_argument('mode', help='run benchmarks and save results, or just read benchmark results from folder, or continue with missing benchmarks only', choices=['resultsets', 'errors', 'warnings', 'query'])
     args = parser.parse_args()
     # path of folder containing experiment results
@@ -78,75 +80,82 @@ if __name__ == '__main__':
             numRun=0
             df = evaluate.get_datastorage_df(numQuery, numRun)
             data = df.values
-            #print(data)
-            #data = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float or tools.convertToFloat(item) == int else item for item in sublist] for sublist in data]
-            #print(data)
             if len(data) > 0 and sum([len(v) for k,v in list_warnings.items()]) > 0:
                 print("\n===Q{}: {}===".format(numQuery, query.title))
                 print(list_warnings)
-                df = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df.columns)
+                pd.set_option('display.max_colwidth', None)
+                pd.set_option('display.max_rows', None)
+                print("Storage:", df)
+                if not args.diff:
+                    df = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df.columns)
+                #print(df)
                 for c in list_connections:
                     if c in list_warnings and len(list_warnings[c])>0:
                         s = evaluate.benchmarks.protocol['query'][str(numQuery)]['dataStorage']
                         r = evaluate.benchmarks.protocol['query'][str(numQuery)]['resultSets']
+                        #print(r[c])
+                        #print(r[c][numRun][0])
+                        #print(s[numRun][0])
+                        print(s)
+                        #exit()
                         for numRun, result in enumerate(s):
                             if args.num_run is not None and int(args.num_run) != numRun:
                                 continue
+                            if not args.diff:
+                                print(c, evaluate.get_resultset_df(numQuery, c, numRun))
+                                continue
                             df2={}
                             data_stored = s[numRun]
+                            if len(data_stored) == 0:
+                                continue
                             if len(r[c]) == 0:
                                 continue
                             print("numRun: "+str(numRun+1))
                             #print(data)
                             #print(data_stored, r[c][numRun])
-                            s2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in data_stored]
+                            s2 = data_stored
+                            r2 = r[c][numRun]
                             #print(s2)
-                            #print(r[c])
-                            r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in r[c][numRun]]
                             #print(r2)
-                            #for c, result_diff in r.items():
-                            #print(convertToInt(r2[0][0]))
-                            #print(type(convertToInt(r2[0][0])))
-                            #print(type(convertToInt(s2[0][0])))
-                            #print(len(s2[0][0].strip()))
-                            #print(type(r2[0][0]))
-                            #print(type(s2[0][0]))
-                            #print(s2[0][0])
-                            #for x in s2[0][0]:
-                            #    print(x)
-                            if len(r2) > 0 and r2 != s2:# r[c][numRun] != data_stored:
-                                #print(c)
-                                #s2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float or tools.convertToFloat(item) == int else item for item in sublist] for sublist in data_stored]
-                                #print(s2)
-                                #r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float or tools.convertToFloat(item) == int else item for item in sublist] for sublist in r[c][numRun]]
-                                #print(r2)
-                                #exit()
-                                df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
-                                data = df_tmp.values
-                                r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in data]
-                                #print(r2)
-                                #print(convertToInt(r2[0][0]))
-                                #print(type(convertToInt(r2[0][0])))
-                                #print(type(convertToInt(s2[0][0])))
-                                #print(len(s2[0][0].strip()))
-                                #print(type(r2[0][0]))
-                                #print(type(s2[0][0]))
-                                #print(s2[1][0])
-                                #for x in s2[0][0]:
-                                #    print(x)
-                                #print(type(ast.literal_eval(s2[0][0])))
-                                #print((tools.convertToFloat(s2[0][0])))
+                            #print(s[numRun], s[numRun][0])
+                            titles_storage = s[numRun][0]
+                            titles_resultset = r[c][numRun][0]
+                            # remove titles
+                            if args.remove_titles:
+                                #s2 = [l.pop(0) for l in s2]
+                                titles_storage = list(range(len(s[numRun][0])))
+                                s2.pop(0)
+                            # remove titles
+                            if args.remove_titles:
+                                #r2 = [l.pop(0) for l in r2]
+                                titles_resultset = list(range(len(r[c][numRun][0])))
+                                r2.pop(0)
+                            # convert datatypes
+                            s2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in s2]
+                            r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in r2]
+                            #print(s2)
+                            #print(r2)
+                            #print(len(s2[0]), titles_storage)
+                            if len(s2) > 0:
+                                df = pd.DataFrame(sorted(s2, key=itemgetter(*list(range(0,len(s2[0]))))), columns=titles_storage)#df_tmp.columns)
+                            #print(df)
+                            if len(r2) > 0 and r2 != s2:
+                                #df_tmp = evaluate.get_resultset_df(numQuery, c, numRun)
+                                #print(df_tmp.columns)
+                                #data = df_tmp.values
+                                #r2 = [[round(float(item), int(query.restrict_precision)) if tools.convertToFloat(item) == float else convertToInt(item) if convertToInt(item) == item else item for item in sublist] for sublist in data]
                                 data = r2
-                                #print(data)
                                 if len(data) > 0:
-                                    df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=df_tmp.columns)
+                                    df2[c] = pd.DataFrame(sorted(data, key=itemgetter(*list(range(0,len(data[0]))))), columns=titles_resultset)#df_tmp.columns)
                                 else:
                                     df2[c] = pd.DataFrame()
                             for c,d in df2.items():
-                                print("Storage has more than {}:".format(c))
-                                print(inspector.getDifference12(df,d))
-                                print("{} has more than storage".format(c))
-                                print(inspector.getDifference12(d,df))
+                                if not inspector.getDifference12(df,d).empty:
+                                    print("Storage has more than {}:".format(c))
+                                    print(inspector.getDifference12(df,d))
+                                if not inspector.getDifference12(d,df).empty:
+                                    print("{} has more than storage".format(c))
+                                    print(inspector.getDifference12(d,df))
                             if len(df2) > 0:
                                 break
     elif args.mode == 'errors':
@@ -179,5 +188,6 @@ if __name__ == '__main__':
             if not df2_warnings.empty:
                 print("===Q{}: {}===".format(numQuery, query.title))
                 print(df2_warnings)
+
 
 
