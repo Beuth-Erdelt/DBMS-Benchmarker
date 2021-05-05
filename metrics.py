a935c5084978930bb1ef73765894bec7f0c9c39b
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2021-05-05 15:24:33 +0200

    Evaluator: Fetch metrics per stream - bexhoma not needed

--- metrics.py
+++ metrics.py
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from bexhoma import *
+#from bexhoma import *
 from dbmsbenchmarker import *
 #import experiments
 import logging
