9a092a496f4839cc3867969a1110cc3d07ccfdbc
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2021-08-31 17:00:14 +0200

    Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Bexhoma'
+copyright = '2021, Patrick Erdelt'
+author = 'Patrick Erdelt'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    "myst_parser",
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+.. toctree::
+    :maxdepth: 2
+    :caption: Table of Contents:
+
+    ../README.md
+    ...
+.. mdinclude:: ../README.md
