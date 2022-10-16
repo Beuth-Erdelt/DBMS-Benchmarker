4f8c73b03c0d03f9d5eac26ee187a92db7b2526a
Patrick Erdelt <patrick.erdelt@beuth-hochschule.de>
2022-10-16 22:52:45 +0200

    Release v0.13.0
    Release v0.13.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="dbmsbenchmarker",
-    version="0.12.5",
+    version="0.13.0",
     author="Patrick Erdelt",
     author_email="perdelt@beuth-hochschule.de",
     description="DBMS-Benchmarker is a Python-based application-level blackbox benchmark tool for Database Management Systems (DBMS). It connects to a given list of DBMS (via JDBC) and runs a given list of parametrized and randomized (SQL) benchmark queries. Evaluations are available via Python interface, in reports and at an interactive multi-dimensional dashboard.",
