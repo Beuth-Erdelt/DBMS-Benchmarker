a7d49caeeaf34f99002e5d6b26ca2f56a3ae9082
Patrick Erdelt <perdelt@bht-berlin.de>
2024-02-04 00:01:45 +0100

    V0.13.6 (#149)
    * fix: requirements.txt to reduce vulnerabilities
    
    
    The following vulnerabilities are fixed by pinning transitive dependencies:
    - https://snyk.io/vuln/SNYK-PYTHON-FONTTOOLS-6133203
    
    * TPC-H: Q15 for MonetDB
    
    * Dashboard: Show errors and warnings again
    
    * pandas: Downcasting behavior in `replace` is deprecated
    
    * Dashboard: Cleaned code
    
    * TPC-H: Stream ordering
    
    * DBMSbenchmarker: Stream ordering
    
    * DBMSbenchmarker:  Series.__getitem__ treating keys as positions is deprecated
    
    * DBMSBenchmarker: Stream ordering in CLI
    
    * DBMSBenchmarker: correct monitoring for shuffled queries
    
    * DBMSBenchmarker: suppress notifocation about missing protocol
    
    * DBMSBenchmarker: suppress notifocation about results folder
    
    * fix: requirements.txt to reduce vulnerabilities
    
    
    The following vulnerabilities are fixed by pinning transitive dependencies:
    - https://snyk.io/vuln/SNYK-PYTHON-DASH-6226335
    
    * Build(deps): Bump dash from 2.14.1 to 2.15.0 (#148)
    
    Bumps [dash](https://github.com/plotly/dash) from 2.14.1 to 2.15.0.
    - [Release notes](https://github.com/plotly/dash/releases)
    - [Changelog](https://github.com/plotly/dash/blob/dev/CHANGELOG.md)
    - [Commits](https://github.com/plotly/dash/compare/v2.14.1...v2.15.0)

--- dbmsbenchmarker/scripts/dashboardcli.py
+++ dbmsbenchmarker/scripts/dashboardcli.py
@@ -51,7 +51,6 @@ logging.basicConfig(level=logging.ERROR)
 
 
 
-print("HERE")
 parser = argparse.ArgumentParser(description='Dashboard for interactive inspection of dbmsbenchmarker results.')
 parser.add_argument('-r', '--result-folder',
                     help='Folder storing benchmark result files.',
@@ -981,7 +980,7 @@ class Graph:
                     df_text = df.replace({0: '', 1: 'error'})
 
                     for index, row in df_text.iterrows():
-                        for column, value in row.iteritems():
+                        for column, value in row.items():
                             if value:
                                 try:
                                     numQuery = int(column.replace('Q', ''))
@@ -1005,7 +1004,7 @@ class Graph:
                     df = df.replace({False: 0, True: 1})
 
                     for index, row in df_text.iterrows():
-                        for column, value in row.iteritems():
+                        for column, value in row.items():
                             if value:
                                 try:
                                     numQuery = int(column.replace('Q', ''))
